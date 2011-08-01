43aef9cc0ec73c8e3a0920222cbff7fda2e7e9f2
Alejandro Leiva <gloob@litio.org>
2011-08-01 13:37:22 +0200

    Creating initial plugin structure.

---
+++ src/accessibility.cpp
@@ -0,0 +1,37 @@
+/*
+ * Compiz Accessibility PLugin
+ *
+ * Copyright (c) 2011 F123.org
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * Author: Alejandro Leiva <aleiva@emergya.com>
+ *
+ */
+
+#include "accessibility.h"
+
+COMPIZ_PLUGIN_20090315 (accessibility, AccessibilityPluginVTable);
+
+
+bool
+AccessibilityPluginVTable::init ()
+{
+    if (!CompPlugin::checkPluginABI ("core", CORE_ABIVERSION))
+    {
+        compLogMessage ("Accessibility", CompLogLevelInfo, "compiz core not in sync.\n");
+        return false;
+    }
+
+    compLogMessage ("example", CompLogLevelInfo, "Running Accessibility plugin.\n");
+    return true;
+}
+
---
+++ src/accessibility.h
@@ -0,0 +1,59 @@
+/*
+ * Compiz Accessibility PLugin
+ *
+ * Copyright (c) 2011 F123.org
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * Author: Alejandro Leiva <aleiva@emergya.com>
+ *
+ */
+
+#include <core/core.h>
+#include <core/pluginclasshandler.h>
+
+#include <dbus/dbus.h>
+
+#include "accessibility_options.h"
+
+class AccessibilityScreen :
+    public PluginClassHandler <AccessibilityScreen, CompScreen>,
+    public ScreenInterface
+{
+    public:
+
+        AccessibilityScreen (CompScreen *);
+        ~AccessibilityScreen ();
+        
+};
+
+class AccessibilityPluginVTable :
+    public CompPlugin::VTableForScreen <AccessibilityScreen>
+{
+    public:
+
+        bool init();
+        
+};
+
+/* Private definitions */
+
+class DBusConnection
+{
+    public:
+        
+        DBusConnection ();
+        ~DBusConnection ();
+        /*
+        DBusSession connect();
+        */
+        
+};
