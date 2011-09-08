b499468d52053a6006e9f77517f762e05541196b
Alejandro Leiva <gloob@litio.org>
2011-09-08 02:44:38 +0200

    Moving AccessibilityEvent public class dependencies to public include.

--- include/accessibility/accessibility.h
+++ include/accessibility/accessibility.h
@@ -20,7 +20,161 @@
 #ifndef _ACCESSIBILITY_H
 #define _ACCESSIBILITY_H
 
-class AccessibilityEvent;
+#ifdef __cplusplus
+extern "C" {
+#endif
+#include <atspi/atspi.h>
+#ifdef __cplusplus
+}
+#endif
+
+/*
+ * Class hierarchy that hold and manage differents accessible objects.
+ */
+enum IfaceType
+{
+    Accessible = 0,
+    Action,
+    Collection,
+    Component,
+    Document,
+    EditableText,
+    Hypertext,
+    Hyperlink,
+    Image,
+    Selection,
+    Table,
+    Text,
+    Value,
+};
+
+class AccessibilityObjectInterface {};
+
+class AccessibilityEntity
+{
+    public:
+        AccessibilityEntity (AtspiAccessible *);
+        ~AccessibilityEntity ();
+
+        virtual bool
+        load (AtspiAccessible *);
+
+        virtual bool
+        contains (AccessibilityEntity *, int, int);
+
+        virtual IfaceType
+        is ();
+
+    protected:
+        AtspiAccessible   *obj;
+};
+
+class AccessibilityComponent :
+    public AccessibilityEntity,
+    public AccessibilityObjectInterface
+{
+    public:
+        AccessibilityComponent (AtspiAccessible *);
+        ~AccessibilityComponent ();
+
+        CompRect
+        getExtents () const;
+        
+        CompPoint
+        getPosition () const;
+
+        CompPoint
+        getSize () const;
+
+        IfaceType
+        is ();
+
+        /* TODO: Implement based in a compiz layer type.
+        CompLayer
+        getLayer ();
+        */
+    protected:
+        AtspiComponent    *component;
+};
+
+class AccessibilityText :
+    public AccessibilityEntity,
+    public AccessibilityObjectInterface
+{
+    public:
+        AccessibilityText (AtspiAccessible *);
+        ~AccessibilityText ();
+
+        CompRect
+        getCharacterExtents (int) const;
+
+        CompRect
+        getRangeExtents (int) const;
+
+        IfaceType
+        is ();
+
+    protected:
+        AtspiText          *text;
+};
+
+class AccessibleObject
+{
+    public:        
+        typedef std::vector <AccessibilityEntity *> Entities;
+        typedef std::vector <IfaceType> Interfaces;
+
+        AccessibleObject (AtspiAccessible *);
+        
+        AccessibleObject::Entities
+        create (AtspiAccessible *);
+
+        AccessibilityEntity *
+        get (IfaceType);
+
+        bool
+        is (IfaceType);
+
+    private:
+        static AccessibilityEntity *
+        instantiate (AtspiAccessible *, char *);
+
+        static IfaceType
+        enumFromStr (const char *);
+
+        int
+        getIfaceIndex (IfaceType);
+
+    private:
+        Entities     ents;
+        Interfaces   interfaces;
+};
+
+class AccessibilityEvent
+{
+    public:
+        //AccessibilityEvent ();
+        AccessibilityEvent (const AtspiEvent *);
+        ~AccessibilityEvent ();
+
+        bool
+        loadEvent (const AtspiEvent *);
+
+        AccessibleObject *
+        getAccessibleObject ();
+
+    public:
+        AtspiAccessible   *obj;
+        char              *name;
+        char              *type;
+        int                detail1;
+        int                detail2;
+
+    private:
+        const AtspiEvent  *event;
+        AccessibleObject  *object;
+
+};
 
 typedef boost::function<void (AccessibilityEvent *)> AccessibilityEventCallback;
 
--- src/private.h
+++ src/private.h
@@ -25,47 +25,6 @@
 #include <accessibility/accessibility.h>
 #include "accessibility_options.h"
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-#include <atspi/atspi.h>
-#ifdef __cplusplus
-}
-#endif
-
-typedef int AccessibilityEventHandler;
-
-/* Struct of a handler and list of handlers */
-struct AccessibilityHandler {
-    const char *                event_type;
-    AtspiEventListener *        event_listener;
-    AccessibilityEventCallback  cb;
-    AccessibilityEventHandler   id;
-};
-
-typedef std::list<AccessibilityHandler *> AccessibilityHandlerList;
-
-
-/*
- * Class hierarchy that hold and manage differents accessible objects.
- */
-enum IfaceType
-{
-    Accessible = 0,
-    Action,
-    Collection,
-    Component,
-    Document,
-    EditableText,
-    Hypertext,
-    Hyperlink,
-    Image,
-    Selection,
-    Table,
-    Text,
-    Value,
-};
-
 static const char *IfaceTypeStr[] =
 {
     "Accessible",
@@ -85,133 +44,17 @@ static const char *IfaceTypeStr[] =
 
 #define NUM_IFACES_SUPPORTED 13
 
-class AccessibilityObjectInterface {};
+typedef int AccessibilityEventHandler;
 
-class AccessibilityEntity
-{
-    public:
-        AccessibilityEntity (AtspiAccessible *);
-        ~AccessibilityEntity ();
-
-        virtual bool
-        load (AtspiAccessible *);
-
-        virtual bool
-        contains (AccessibilityEntity *, int, int);
-
-        virtual IfaceType
-        is ();
-
-    protected:
-        AtspiAccessible   *obj;
+/* Struct of a handler and list of handlers */
+struct AccessibilityHandler {
+    const char *                event_type;
+    AtspiEventListener *        event_listener;
+    AccessibilityEventCallback  cb;
+    AccessibilityEventHandler   id;
 };
 
-class AccessibilityComponent :
-    public AccessibilityEntity,
-    public AccessibilityObjectInterface
-{
-    public:
-        AccessibilityComponent (AtspiAccessible *);
-        ~AccessibilityComponent ();
-
-        CompRect
-        getExtents () const;
-        
-        CompPoint
-        getPosition () const;
-
-        CompPoint
-        getSize () const;
-
-        IfaceType
-        is ();
-
-        /* TODO: Implement based in a compiz layer type.
-        CompLayer
-        getLayer ();
-        */
-    protected:
-        AtspiComponent    *component;
-};
-
-class AccessibilityText :
-    public AccessibilityEntity,
-    public AccessibilityObjectInterface
-{
-    public:
-        AccessibilityText (AtspiAccessible *);
-        ~AccessibilityText ();
-
-        CompRect
-        getCharacterExtents (int) const;
-
-        CompRect
-        getRangeExtents (int) const;
-
-        IfaceType
-        is ();
-
-    protected:
-        AtspiText          *text;
-};
-
-class AccessibleObject
-{
-    public:        
-        typedef std::vector <AccessibilityEntity *> Entities;
-        typedef std::vector <IfaceType> Interfaces;
-
-        AccessibleObject (AtspiAccessible *);
-        
-        AccessibleObject::Entities
-        create (AtspiAccessible *);
-
-        AccessibilityEntity *
-        get (IfaceType);
-
-        bool
-        is (IfaceType);
-
-    private:
-        static AccessibilityEntity *
-        instantiate (AtspiAccessible *, char *);
-
-        static IfaceType
-        enumFromStr (const char *);
-
-        int
-        getIfaceIndex (IfaceType);
-
-    private:
-        Entities     ents;
-        Interfaces   interfaces;
-};
-
-class AccessibilityEvent
-{
-    public:
-        //AccessibilityEvent ();
-        AccessibilityEvent (const AtspiEvent *);
-        ~AccessibilityEvent ();
-
-        bool
-        loadEvent (const AtspiEvent *);
-
-        AccessibleObject *
-        getAccessibleObject ();
-
-    public:
-        AtspiAccessible   *obj;
-        char              *name;
-        char              *type;
-        int                detail1;
-        int                detail2;
-
-    private:
-        const AtspiEvent  *event;
-        AccessibleObject  *object;
-
-};
+typedef std::list<AccessibilityHandler *> AccessibilityHandlerList;
 
 class AccessibilityScreen :
     public PluginClassHandler <AccessibilityScreen, CompScreen>,
